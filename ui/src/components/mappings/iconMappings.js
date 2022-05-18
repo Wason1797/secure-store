11c9ca43355969115f414b5b274478b7d8d8b26b
Wladymir Brborich <wabrborich@hotmail.com>
2022-05-17 23:40:00 -0500

    Create an icon mapping dictionary
    * This is to not have to import a bunch of icons on every component that needs them

---
+++ ui/src/components/mappings/iconMappings.js
@@ -0,0 +1,25 @@
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import PeopleIcon from "@mui/icons-material/People";
+import BarChartIcon from "@mui/icons-material/BarChart";
+import LayersIcon from "@mui/icons-material/Layers";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+import Logout from "@mui/icons-material/Logout";
+import Key from "@mui/icons-material/Key";
+import UploadFileIcon from "@mui/icons-material/UploadFile";
+import GoogleIcon from '@mui/icons-material/Google';
+
+const IconMapping = Object.freeze({
+    Dashboard: DashboardIcon,
+    ShoppingCart: ShoppingCartIcon,
+    People: PeopleIcon,
+    BarChart: BarChartIcon,
+    Layers: LayersIcon,
+    Assignment: AssignmentIcon,
+    Logout: Logout,
+    Key: Key,
+    UploadFileIcon: UploadFileIcon,
+    Google: GoogleIcon
+});
+
+export default IconMapping;
