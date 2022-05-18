a6fb0b7de4d0f6c015f699e6d4bff35227d50964
Wladymir Brborich <wabrborich@hotmail.com>
2022-05-17 23:32:51 -0500

    Add endpoints to get all avilable users

---
+++ api/app/routers/users.py
@@ -0,0 +1,15 @@
+from typing import Optional
+
+from app.repositories.database.local import LocalStorage
+from fastapi import APIRouter, Depends
+
+from ..security.validators import get_user
+
+router = APIRouter()
+
+
+@router.get('/')
+async def get_users(user: Optional[dict] = Depends(get_user)):
+    users = tuple(LocalStorage.get_storage().get_data().keys())
+
+    return {'users': users}
