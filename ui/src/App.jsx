2bc663aa4921d4eef515fc34e4f3da774abbffd7
Wladymir Brborich <wabrborich@hotmail.com>
2022-05-19 02:14:21 -0500

    Add global css baseline to app

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import UpdateUserKey from "./pages/UpdateUserKey";
+import CssBaseline from "@mui/material/CssBaseline";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <Router>
+      <CssBaseline />
       <Routes>
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/update-user-key" element={<UpdateUserKey />} />
