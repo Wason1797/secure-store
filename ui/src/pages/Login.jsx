e74f9a2a4ec9f92e3e8539c2394d3cef5415df44
Wladymir Brborich <wabrborich@hotmail.com>
2022-05-19 00:52:51 -0500

    Implement Login functionality
    * add login action to the button
    
    * fix project name in title
    
    * improve component styling
    
    * add a loading section for the component
    
    * check if the user is already logged in to redirect it to the dashboard

--- ui/src/pages/Login.jsx
+++ ui/src/pages/Login.jsx
@@ -6,51 +6,72 @@ import Button from "@mui/material/Button";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import IconMapping from "../components/mappings/iconMappings";
+import EnvManager from "../config/envManager";
+import { didUserActivateSession } from "../services/api/users";
+import LoadingSection from "../components/sections/LoadingSection";
+import { useNavigate } from "react-router-dom";
 
 const mdTheme = createTheme();
 
+const loginUrl = `${EnvManager.BACKEND_URL}/auth/login`;
+
 const Login = (props) => {
   const GoogleIcon = IconMapping["Google"];
+  const [isLoading, setLoading] = React.useState(true);
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    didUserActivateSession().then((isSessionActive) => {
+      if (isSessionActive) navigate("/dashboard");
+      setLoading(false);
+    });
+  }, [navigate]);
+
   return (
     <ThemeProvider theme={mdTheme}>
       <CssBaseline />
-      <Grid
-        container
-        spacing={0}
-        direction="column"
-        alignItems="center"
-        justifyContent="center"
-        style={{ minHeight: "100vh" }}
-        sx={{
-          backgroundColor: (theme) =>
-            theme.palette.mode === "light" ? theme.palette.grey[100] : theme.palette.grey[900],
-        }}
-      >
-        <Grid item xs={3}>
-          <Paper>
-            <Grid
-              item
-              container
-              direction="column"
-              alignItems="center"
-              justifyContent="space-evenly"
-              spacing={1}
-              style={{ minHeight: "50vh", maxWidth: "30vw", minWidth: "25vw" }}
-            >
-              <Grid item xs={4}>
-                <Typography variant="h2" color="primary" gutterBottom>
-                  Safe Store
-                </Typography>
+      {isLoading ? (
+        <LoadingSection />
+      ) : (
+        <Grid
+          container
+          spacing={0}
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+          style={{ minHeight: "100vh" }}
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === "light" ? theme.palette.grey[100] : theme.palette.grey[900],
+          }}
+        >
+          <Grid item xs={3}>
+            <Paper>
+              <Grid
+                item
+                container
+                direction="column"
+                alignItems="center"
+                justifyContent="space-evenly"
+                spacing={1}
+                p={2}
+                style={{ minHeight: "50vh", maxWidth: "50vw", minWidth: "20vw" }}
+              >
+                <Grid item xs={4}>
+                  <Typography variant="h2" color="primary" align="center" gutterBottom>
+                    Secure Store
+                  </Typography>
+                </Grid>
+                <Grid item xs={8}>
+                  <Button variant="contained" size="large" href={loginUrl} endIcon={<GoogleIcon />}>
+                    Log In
+                  </Button>
+                </Grid>
               </Grid>
-              <Grid item xs={8}>
-                <Button variant="contained" endIcon={<GoogleIcon />}>
-                  Log In
-                </Button>
-              </Grid>
-            </Grid>
-          </Paper>
+            </Paper>
+          </Grid>
         </Grid>
-      </Grid>
+      )}
     </ThemeProvider>
   );
 };
