7959696e18224adcc69a64a74059897064788285
Wladymir Brborich <wabrborich@hotmail.com>
2022-05-19 00:49:15 -0500

    Add frontend url to environment variables

--- api/app/config/env_manager.py
+++ api/app/config/env_manager.py
@@ -6,6 +6,7 @@ class EnvManager:
     GOOGLE_CLIENT_ID: str = os.environ['GOOGLE_CLIENT_ID']
     GOOGLE_CLIENT_SECRET: str = os.environ['GOOGLE_CLIENT_SECRET']
     SESSION_SECRET: str = os.environ['SESSION_SECRET']
+    SECURE_STORE_UI_URL: str = os.environ['SECURE_STORE_UI_URL']
 
     @classmethod
     def get(cls, key, default=None):
