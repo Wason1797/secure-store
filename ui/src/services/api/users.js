2cf03f07db0dea3ac57ddab4bfca751eb83f1a50
Wladymir Brborich <wabrborich@hotmail.com>
2022-05-20 21:34:59 -0500

    use session storage correctly when user logs in

--- ui/src/services/api/users.js
+++ ui/src/services/api/users.js
@@ -1,45 +1,54 @@
 import axios from "axios";
-import EnvManager from '../../config/envManager';
+import EnvManager from "../../config/envManager";
 
 const baseUrl = `${EnvManager.BACKEND_URL}/users`;
 
 const getUsers = async () => {
-
-    const response = await axios.get(baseUrl, {
-        withCredentials: true,
-    });
-    return response?.data.users;
-
+  const response = await axios.get(baseUrl, {
+    withCredentials: true,
+  });
+  return response?.data.users;
 };
 
 const getActiveUserSession = async () => {
-    try {
-        const response = await axios.get(`${baseUrl}/session/active`, {
-            withCredentials: true,
-        });
-        return response?.data;
-    }
-    catch (error) {
-        return null;
-    }
+  try {
+    const response = await axios.get(`${baseUrl}/session/active`, {
+      withCredentials: true,
+    });
+    return response?.data;
+  } catch (error) {
+    return null;
+  }
 };
 
+const getUserFromSessionStorage = () => {
+  if (!sessionStorage.getItem("isUserActive")) return null;
+  return {
+    name: sessionStorage.getItem("userName"),
+    exp: sessionStorage.getItem("userExp"),
+    email: sessionStorage.getItem("userEmail"),
+    picture: sessionStorage.getItem("userPicture"),
+  };
+};
 
+const setUserInSessionStorage = (user, isActive) => {
+  sessionStorage.setItem("isUserActive", isActive);
+  sessionStorage.setItem("userName", user.name);
+  sessionStorage.setItem("userExp", user.exp);
+  sessionStorage.setItem("userEmail", user.email);
+  sessionStorage.setItem("userPicture", user.picture);
+};
 
 const didUserActivateSession = async () => {
-    const sessionUser = sessionStorage.getItem('user');
-    if (sessionUser && sessionUser.exp < new Date().getTime()) return true;
-    const user = await getActiveUserSession();
-    if (user) {
-        sessionStorage.setItem('user', user);
-        return user;
-    }
-    return null;
+  const sessionUser = getUserFromSessionStorage();
+
+  if (sessionUser && sessionUser.exp < new Date().getTime()) return sessionUser;
+  const user = await getActiveUserSession();
+  if (user) {
+    setUserInSessionStorage(user, true);
+    return user;
+  }
+  return null;
 };
 
-
-
-export {
-    getUsers,
-    didUserActivateSession
-};
+export { getUsers, didUserActivateSession };
