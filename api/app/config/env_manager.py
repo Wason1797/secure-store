65bd28ae2b0edce4fda9b8dd9475cfb6a64e2e69
Wladymir Brborich <wabrborich@hotmail.com>
2022-05-20 20:04:55 -0500

    add new config variables to project

--- api/app/config/env_manager.py
+++ api/app/config/env_manager.py
@@ -7,6 +7,11 @@ class EnvManager:
     GOOGLE_CLIENT_SECRET: str = os.environ['GOOGLE_CLIENT_SECRET']
     SESSION_SECRET: str = os.environ['SESSION_SECRET']
     SECURE_STORE_UI_URL: str = os.environ['SECURE_STORE_UI_URL']
+    AWS_ACCESS_KEY_ID: str = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY: str = os.getenv('AWS_SECRET_ACCESS_KEY')
+    AWS_ENDPOINT: str = os.getenv('AWS_ENDPOINT')
+    S3_BUCKET_NAME: str = os.environ['S3_BUCKET_NAME']
+    TEMP_FOLDER: str = os.getenv('TEMP_FOLDER', './temp')
 
     @classmethod
     def get(cls, key, default=None):
