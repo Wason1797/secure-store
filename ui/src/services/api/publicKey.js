09a3bb102745d09ea2dbe60889b382f4b29ac2ab
Wladymir Brborich <wabrborich@hotmail.com>
2022-05-29 22:13:36 -0500

    Add service to update the user public key
    * This service updates the public key destructively, any new content will be encrypted with the new key

---
+++ ui/src/services/api/publicKey.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import EnvManager from '../../config/envManager';
+
+const baseUrl = EnvManager.BACKEND_URL;
+
+const uploadPublicKey = async (publicKeyFile, onUploadProgress, done) => {
+    try {
+        const data = new FormData();
+        data.append('public_key', publicKeyFile);
+        const response = await axios.post(`${baseUrl}/public-key/upload`, data,
+            {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                },
+                withCredentials: true
+            },
+            onUploadProgress
+        );
+        done();
+        return response?.data;
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+};
+
+export {
+    uploadPublicKey,
+};
