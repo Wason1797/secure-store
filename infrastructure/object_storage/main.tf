a1afc750183eac5af32f5e42faeb7ebe3a5054bd
Wladymir Brborich <wabrborich@hotmail.com>
2022-05-31 12:04:22 -0500

    update resource name for bucket acl

--- infrastructure/object_storage/main.tf
+++ infrastructure/object_storage/main.tf
@@ -8,7 +8,7 @@ resource "aws_s3_bucket" "public-key-bucket" {
   }
 }
 
-resource "aws_s3_bucket_acl" "example" {
+resource "aws_s3_bucket_acl" "public-key-bucket-acl" {
   bucket = aws_s3_bucket.public-key-bucket.id
   acl    = "private"
 }
