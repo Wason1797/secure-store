3a3291d6f798fa5a1d77e6b4f59b1142cfb602d5
Wladymir Brborich <wabrborich@hotmail.com>
2022-06-01 17:53:28 -0500

    Improve the users router with new persistence layer
    * use the new manager to retrieve all avilable users
    
    * use the new serializers to accurately control the responses

--- api/app/routers/users.py
+++ api/app/routers/users.py
@@ -1,6 +1,9 @@
-from typing import Optional
+from typing import List, Optional
 
-from app.repositories.database.connectors.local import LocalStorage
+from app.repositories.database.connectors import DynamoDBConnector
+from app.repositories.database.managers.user import UserManager
+from app.serializers.user import SessionUser as SessionUserSerializer
+from app.serializers.user import UserBase as UserBaseSerializer
 from fastapi import APIRouter, Depends
 
 from ..security.validators import get_user
@@ -8,18 +11,12 @@ from ..security.validators import get_user
 router = APIRouter()
 
 
-@router.get('/')
-async def get_users(user: Optional[dict] = Depends(get_user)):
-    users = tuple(LocalStorage.get_storage().get_data().keys())
+@router.get('/', response_model=List[UserBaseSerializer])
+async def get_users(user: Optional[dict] = Depends(get_user), db=Depends(DynamoDBConnector.get_db)):
+    users = await UserManager.get_all(db)
+    return users
 
-    return {'users': users}
 
-
-@router.get('/session/active')
+@router.get('/session/active', response_model=SessionUserSerializer)
 async def get_active_session_user(user: Optional[dict] = Depends(get_user)):
-    return {
-        'name': user.get('name'),
-        'exp': user.get('exp'),
-        'email': user.get('email'),
-        'picture': user.get('picture'),
-    }
+    return SessionUserSerializer(**user)
