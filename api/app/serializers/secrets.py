a18762c1153d8e6615f13bdd5d195cc0b2cea55e
Wladymir Brborich <wabrborich@hotmail.com>
2022-06-02 20:16:30 -0500

    Add secrets serializers
    * This are to properly return the api values when sharing secrets

--- api/app/serializers/secrets.py
+++ api/app/serializers/secrets.py
@@ -6,3 +6,16 @@ from pydantic.networks import EmailStr
 class ShareSecretsPayload(BaseModel):
     users: List[EmailStr]
     secrets: dict
+
+
+class SecretBase(BaseModel):
+    secret_id: str
+    recipient_email: EmailStr
+    created_at: int
+    secret: str
+    sender_email: EmailStr
+
+
+class SecretFull(SecretBase):
+    recipient_sub: str
+    sender_sub: str
