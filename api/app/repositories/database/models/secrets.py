ace8a0a4294625467a41eb13b6c78fdf65cea2d6
Wladymir Brborich <wabrborich@hotmail.com>
2022-06-02 20:13:27 -0500

    correct Secret model
    * delete duplicated value
    
    * use secret_id as the correct key defined in the database

--- api/app/repositories/database/models/secrets.py
+++ api/app/repositories/database/models/secrets.py
@@ -4,11 +4,10 @@ from dataclasses import dataclass, asdict
 @dataclass
 class Secret:
 
+    secret_id: str
     recipient_sub: str
     recipient_email: str
-    recipient_email: str
     created_at: int
-    secret_identifier: str
     secret: str
     sender_sub: str
     sender_email: str
