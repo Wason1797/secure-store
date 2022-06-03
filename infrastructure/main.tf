c0a0729906dfbbc190959baff8a59972d3cd7f13
Wladymir Brborich <wabrborich@hotmail.com>
2022-06-03 18:45:25 -0500

    Add support for the new cache modules
    * Unfortunately to implement a more secure API we need to add another persistence layer
    
    * This cache layer will allow us to store ecdh secrets across api calls
    
    * Ideally they will be encrypted by a server key

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,8 +4,15 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.9.0"
     }
+
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.16.0"
+    }
   }
 }
+
+provider "docker" {}
 
 provider "aws" {
   region     = var.REGION
@@ -14,12 +21,23 @@ provider "aws" {
 }
 
 module "db" {
-  source = "./db"
-  USER_TABLE_NAME = "users"
+  source             = "./db"
+  USER_TABLE_NAME    = "users"
   SECRETS_TABLE_NAME = "secrets"
 }
 
 module "object-storage" {
-  source = "./object_storage"
+  source                 = "./object_storage"
   PUBLIC_KEY_BUCKET_NAME = var.PUBLIC_KEY_BUCKET_NAME
 }
+
+module "cache" {
+  source        = "./cache"
+  create_module = (var.ENV == "PROD") ? 1 : 0
+}
+
+module "local_cache" {
+  source        = "./local_cache"
+  create_module = (var.ENV == "DEV") ? 1 : 0
+}
+
