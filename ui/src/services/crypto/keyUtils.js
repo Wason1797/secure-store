3b39ad96442ac074635afed019306a03b3b6bdc9
Wladymir Brborich <wabrborich@hotmail.com>
2022-06-04 11:21:04 -0500

    Refactor the key utils file
    * Include actual key utils in this file ie: wrap the subtle crypto api into a more readable form
    
    * Remove the previous rsa functions from the file

--- ui/src/services/crypto/keyUtils.js
+++ ui/src/services/crypto/keyUtils.js
@@ -1,29 +1,25 @@
-import * as crypto from "crypto-browserify";
+import { bufferToHexString, hexStringToArray } from './helpers';
 
-const decryptWithPrivateKey = (privateKey, secret) => {
-    const decryptedData = crypto.privateDecrypt(
-        {
-            key: Buffer.from(privateKey),
-            padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-            oaepHash: "sha256",
-        },
-        Buffer.from(secret, "base64")
-    );
-    return decryptedData.toString("utf-8");
+const subtleCrypto = window.crypto.subtle;
+
+const generateECDHPrivateKey = async (keyGenParams = { name: 'ECDH', namedCurve: 'p-256' }, exportable = false) =>
+    subtleCrypto.generateKey(keyGenParams, exportable, ['deriveKey', 'deriveBits']);
+
+
+const getPublicKey = async (privateKey, format = 'raw') => {
+    const rawPublicKey = subtleCrypto.exportKey(format, privateKey.publiKey);
+    return bufferToHexString(rawPublicKey);
 };
 
-const decryptSecretsWithPrivateKeyFile = async (privateKey, secrets) => {
-    if (!privateKey || !secrets) return;
-    const privateKeyArrayBuffer = await privateKey.arrayBuffer();
-    return secrets.map((secretData) => ({
-        ...secretData,
-        secret: decryptWithPrivateKey(privateKeyArrayBuffer, secretData.secret),
-    }));
+const importECDHPublicKey = async (publicKeyHexString, format = 'spki', keyParams = { name: 'ECDH', namedCurve: 'p-256' }, exportable = true) => {
+    const publicKey = hexStringToArray(publicKeyHexString);
+    return subtleCrypto.importKey(format, publicKey, keyParams, exportable, []);
 };
 
 
 
 export {
-    decryptWithPrivateKey,
-    decryptSecretsWithPrivateKeyFile
+    getPublicKey,
+    generateECDHPrivateKey,
+    importECDHPublicKey,
 };
