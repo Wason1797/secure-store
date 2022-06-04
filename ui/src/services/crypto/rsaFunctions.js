16c98d79e16fe45382e793af9ff1037197c2338d
Wladymir Brborich <wabrborich@hotmail.com>
2022-06-04 11:21:51 -0500

    Create a dedicated file for rsa functions
    * This file contains all the logic to perform rsa private key decryption with the browserified crypto module

---
+++ ui/src/services/crypto/rsaFunctions.js
@@ -0,0 +1,28 @@
+import * as crypto from "crypto-browserify";
+
+const decryptWithPrivateKey = (privateKey, secret) => {
+    const decryptedData = crypto.privateDecrypt(
+        {
+            key: Buffer.from(privateKey),
+            padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+            oaepHash: 'sha256',
+        },
+        Buffer.from(secret, 'base64')
+    );
+    return decryptedData.toString('utf-8');
+};
+
+const decryptSecretsWithPrivateKeyFile = async (privateKey, secrets) => {
+    if (!privateKey || !secrets) return;
+    const privateKeyArrayBuffer = await privateKey.arrayBuffer();
+    return secrets.map((secretData) => ({
+        ...secretData,
+        secret: decryptWithPrivateKey(privateKeyArrayBuffer, secretData.secret),
+    }));
+};
+
+
+export {
+    decryptWithPrivateKey,
+    decryptSecretsWithPrivateKeyFile
+};
