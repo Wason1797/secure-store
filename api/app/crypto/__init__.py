bcaa9d0b7f183cbf602bf885afad5581a0e3b3a8
Wladymir Brborich <wabrborich@hotmail.com>
2022-06-07 12:07:06 -0500

    Add fernet and aes functions to __init__

--- api/app/crypto/__init__.py
+++ api/app/crypto/__init__.py
@@ -1,3 +1,5 @@
 # flake8: noqa
-from .rsa_functions import RSAEncryption
+from .aes_functions import AESEncryption
 from .ecdh_functions import ECDHExchange
+from .fernet_functions import FernetEncryption
+from .rsa_functions import RSAEncryption
