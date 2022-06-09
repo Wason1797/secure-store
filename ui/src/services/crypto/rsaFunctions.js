12105a7d2b51f0bec1ffd47d13baa818dc184eaf
Wladymir Brborich <wabrborich@hotmail.com>
2022-06-09 18:14:27 -0500

    Add generateRSAKeyPair() to create rsa keys in the app
    * Also refactor decryptWithPrivateKey() so it fails more gracefully. There are some cases where the user has different public keys, corresponding to different private keys, so we cannot decrypt all secrets at once. For now we just quietly fail, in the future we may want to let the user select which secrets belong to which key

--- ui/src/services/crypto/rsaFunctions.js
+++ ui/src/services/crypto/rsaFunctions.js
@@ -1,15 +1,24 @@
 import * as crypto from "crypto-browserify";
 
+const subtleCrypto = window.crypto.subtle;
+
 const decryptWithPrivateKey = (privateKey, secret) => {
-    const decryptedData = crypto.privateDecrypt(
-        {
-            key: Buffer.from(privateKey),
-            padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-            oaepHash: 'sha256',
-        },
-        Buffer.from(secret, 'base64')
-    );
-    return decryptedData.toString('utf-8');
+    try {
+        const decryptedData = crypto.privateDecrypt(
+            {
+                key: Buffer.from(privateKey),
+                padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+                oaepHash: 'sha256',
+            },
+            Buffer.from(secret, 'base64')
+        );
+        return decryptedData.toString('utf-8');
+    }
+    catch (error) {
+        console.log('Error ', error);
+        return '';
+    }
+
 };
 
 const decryptSecretsWithPrivateKeyFile = async (privateKey, secrets) => {
@@ -21,8 +30,34 @@ const decryptSecretsWithPrivateKeyFile = async (privateKey, secrets) => {
     }));
 };
 
+const getKeyStart = (publicOrPrivate) => `-----BEGIN ${publicOrPrivate} KEY-----\n`;
+const getKeyEnd = (publicOrPrivate) => `\n-----END ${publicOrPrivate} KEY-----`;
+
+const generateRSAKeyPair = async (keyLength = 1024) => {
+
+    const rsaKeyPair = await subtleCrypto.generateKey(
+        {
+            name: 'RSA-OAEP',
+            modulusLength: keyLength,
+            publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+            hash: { name: 'SHA-256' },
+        },
+        true,
+        ['encrypt', 'decrypt']
+    );
+    const [rsaPublicKey, rsaPrivateKey] = await Promise.all(
+        [subtleCrypto.exportKey('spki', rsaKeyPair.publicKey),
+        subtleCrypto.exportKey('pkcs8', rsaKeyPair.privateKey)]
+    );
+    return {
+        rsaPublicKey: `${getKeyStart('PUBLIC')}${Buffer.from(rsaPublicKey).toString('base64')}${getKeyEnd('PUBLIC')}`,
+        rsaPrivateKey: `${getKeyStart('PRIVATE')}${Buffer.from(rsaPrivateKey).toString('base64')}${getKeyEnd('PRIVATE')}`
+    };
+};
+
 
 export {
+    generateRSAKeyPair,
     decryptWithPrivateKey,
     decryptSecretsWithPrivateKeyFile
 };
