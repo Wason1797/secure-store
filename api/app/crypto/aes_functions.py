4c8c28682bca0a97809afaa1e7c03dd3634b339d
Wladymir Brborich <wabrborich@hotmail.com>
2022-06-12 19:09:40 -0500

    implement AES decryption functions
    * This functions will allow us to decrypt data using the shared key derived from a key exchange with the frontend

--- api/app/crypto/aes_functions.py
+++ api/app/crypto/aes_functions.py
@@ -1,4 +1,19 @@
+from typing import Union
+
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
 
 class AESEncryption:
-    pass
+
+    @staticmethod
+    def decode_and_strip(decrypted_data: bytes) -> str:
+        return ''.join(character for character in decrypted_data.decode() if character.isprintable())
+
+    @classmethod
+    def decrypt(cls, key: str, iv: str, data: str, raw: bool = False) -> Union[str, bytes]:
+        key = bytes.fromhex(key)
+        iv = bytes.fromhex(iv)
+        cipher = Cipher(algorithms.AES(key), modes.CBC(iv))
+        decryptor = cipher.decryptor()
+        decrypted_data = decryptor.update(bytes.fromhex(data)) + decryptor.finalize()
+        return decrypted_data if raw else cls.decode_and_strip(decrypted_data)
