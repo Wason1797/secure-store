b673486e13297743f64c727730eae2b72ac82ca3
Wladymir Brborich <wabrborich@hotmail.com>
2022-06-13 16:23:05 -0500

    Create a bootstrap shell script
    * Add a shel script to bootstrap all requirements and virtual environments needed for this project
    
    * This is the most basic version and could be significantly improved

---
+++ tooling/environments/bootstrap.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+green="\033[0;32m"
+no_color="\033[0m"
+
+# Create api virtualenv and install requirements
+echo -e "${green}Create api virtualenv${no_color}\n"
+cd ./api
+python3 -m venv venv
+source venv/bin/activate
+echo -e "${green}Install api requirements${no_color}\n"
+python3 -m pip install --upgrade pip
+pip install -r requirements.txt
+deactivate
+
+# Create localstack virtualenv and install requirements
+echo -e "${green}Create localstack virtualenv${no_color}\n"
+python3 -m venv .localStackVenv
+source .localStackVenv/bin/activate
+echo -e "${green}Install localstack requirements${no_color}\n"
+python3 -m pip install --upgrade pip
+pip install -r localstack-requirements.txt
+deactivate
+
+# Create terraform local virtualenv and install requirements
+echo -e "${green}Create tflocal virtualenv${no_color}\n"
+cd ..
+cd ./infrastructure
+python3 -m venv .tflocal
+source .tflocal/bin/activate
+echo -e "${green}Install tflocal requirements${no_color}\n"
+python3 -m pip install --upgrade pip
+pip install -r tflocal-requirements.txt
+deactivate
+
+# Install ui dependencies
+echo -e "${green}Install ui dependencies${no_color}\n"
+cd ..
+cd ./ui
+npm install
+cd ..
