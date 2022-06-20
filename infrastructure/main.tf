11c6deecdfb8a85071c147c65ecf6abbafb6e1d4
Wladymir Brborich <wabrborich@hotmail.com>
2022-06-20 18:40:00 -0500

    add all necesary modules to main terraform script

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -12,28 +12,85 @@ terraform {
   }
 }
 
-provider "docker" {}
+provider "docker" {
+  registry_auth {
+    address  = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.REGION}.amazonaws.com"
+    username = data.aws_ecr_authorization_token.token.user_name
+    password = data.aws_ecr_authorization_token.token.password
+  }
+}
 
 provider "aws" {
   region     = var.REGION
-  access_key = var.ACCESS_KEY
-  secret_key = var.SECRET_KEY
+  access_key = var.TF_AWS_ACCESS_KEY
+  secret_key = var.TF_AWS_SECRET_KEY
+  token      = var.TF_AWS_SESSION_TOKEN
 }
+
+data "aws_ecr_authorization_token" "token" {}
+data "aws_caller_identity" "current" {}
 
 module "db" {
   source             = "./db"
-  USER_TABLE_NAME    = "users"
-  SECRETS_TABLE_NAME = "secrets"
+  user_table_name    = "users"
+  secrets_table_name = "secrets"
 }
 
 module "object-storage" {
   source                 = "./object_storage"
-  PUBLIC_KEY_BUCKET_NAME = var.PUBLIC_KEY_BUCKET_NAME
+  public_key_bucket_name = var.PUBLIC_KEY_BUCKET_NAME
 }
+
+locals {
+  availability_zones = ["${var.REGION}a", "${var.REGION}b", "${var.REGION}c"]
+}
+
+module "vpc" {
+  source             = "./vpc"
+  env                = var.ENV
+  create_module      = (var.ENV == "PROD") ? 1 : 0
+  availability_zones = local.availability_zones
+}
+
+
+
+module "api" {
+  source = "./api"
+  depends_on = [
+    module.vpc
+  ]
+  env             = var.ENV
+  create_module   = (var.ENV == "PROD") ? 1 : 0
+  vpc_id          = module.vpc.vpc_id
+  subnet          = module.vpc.subnet
+  region          = var.REGION
+  secrets_manager = module.secrets_manager.secrets_repository
+  alb_sg_group_id = module.load_balancer.alb_sg_group_id
+  providers = {
+    docker = docker
+  }
+}
+
+module "load_balancer" {
+  source = "./alb"
+  depends_on = [
+    module.vpc
+  ]
+  env             = var.ENV
+  create_module   = (var.ENV == "PROD") ? 1 : 0
+  vpc_id          = module.vpc.vpc_id
+  subnet          = module.vpc.subnet
+  ec2_instance_id = module.api.ec2_instance_id
+}
+
 
 module "cache" {
   source        = "./cache"
   create_module = (var.ENV == "PROD") ? 1 : 0
+  vpc_id        = module.vpc.vpc_id
+  subnet        = module.vpc.subnet
+  ec2_sg_id     = module.api.ec2_sg_id
+  env           = var.ENV
 }
 
 module "local_cache" {
@@ -41,3 +98,18 @@ module "local_cache" {
   create_module = (var.ENV == "DEV") ? 1 : 0
 }
 
+module "secrets_manager" {
+  source                = "./secrets"
+  create_module         = (var.ENV == "PROD") ? 1 : 0
+  oauth_config_url      = var.OAUTH_CONFIG_URL
+  google_client_id      = var.GOOGLE_CLIENT_ID
+  google_client_secret  = var.GOOGLE_CLIENT_SECRET
+  server_salt           = var.SERVER_SALT
+  ui_url                = var.UI_URL
+  aws_access_key_id     = var.AWS_ACCESS_KEY_ID
+  aws_secret_access_key = var.AWS_SECRET_ACCESS_KEY
+  region                = var.REGION
+  bucket_name           = var.PUBLIC_KEY_BUCKET_NAME
+  memory_db_url         = "rediss://default:${module.cache.auth_token}@${module.cache.elasticache_url}:6379"
+  env                   = var.ENV
+}
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,5 +1,13 @@
 variable "REGION" {}
 variable "PUBLIC_KEY_BUCKET_NAME" {}
 variable "ENV" {}
-variable "ACCESS_KEY" { sensitive = true }
-variable "SECRET_KEY" { sensitive = true }
+variable "TF_AWS_ACCESS_KEY" { sensitive = true }
+variable "TF_AWS_SECRET_KEY" { sensitive = true }
+variable "TF_AWS_SESSION_TOKEN" { sensitive = true }
+variable "OAUTH_CONFIG_URL" { sensitive = true }
+variable "GOOGLE_CLIENT_ID" { sensitive = true }
+variable "GOOGLE_CLIENT_SECRET" { sensitive = true }
+variable "SERVER_SALT" { sensitive = true }
+variable "UI_URL" { sensitive = true }
+variable "AWS_ACCESS_KEY_ID" { sensitive = true }
+variable "AWS_SECRET_ACCESS_KEY" { sensitive = true }
